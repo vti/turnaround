b889aebabcaa4885754ebada15c03200b29ec15f
vti <viacheslav.t@gmail.com>
2011-11-05 14:56:41 +0200

    Added ACL

---
+++ lib/Lamework/ACL.pm
@@ -0,0 +1,59 @@
+package Lamework::ACL;
+
+use strict;
+use warnings;
+
+use base 'Lamework::Base';
+
+use List::Util qw(first);
+
+sub add_role {
+    my $self = shift;
+    my ($role, @parents) = @_;
+
+    $self->{roles}->{$role} = {allow => [], deny => []};
+
+    foreach my $parent (@parents) {
+        push @{$self->{roles}->{$role}->{deny}},
+          @{$self->{roles}->{$parent}->{deny}};
+        push @{$self->{roles}->{$role}->{allow}},
+          @{$self->{roles}->{$parent}->{allow}};
+    }
+
+    return $self;
+}
+
+sub allow {
+    my $self = shift;
+    my ($role, $action) = @_;
+
+    die 'Unknown role' unless exists $self->{roles}->{$role};
+
+    push @{$self->{roles}->{$role}->{allow}}, $action;
+}
+
+sub deny {
+    my $self = shift;
+    my ($role, $action) = @_;
+
+    die 'Unknown role' unless exists $self->{roles}->{$role};
+
+    push @{$self->{roles}->{$role}->{deny}}, $action;
+}
+
+sub is_allowed {
+    my $self = shift;
+    my ($role, $action) = @_;
+
+    return 0 unless exists $self->{roles}->{$role};
+
+    return 0 if first { $_ eq $action } @{$self->{roles}->{$role}->{deny}};
+
+    return 1
+      if first { $_ eq $action || $_ eq '*' }
+        @{$self->{roles}->{$role}->{allow}};
+
+    return 0;
+}
+
+1;
---
+++ t/lib/ACLTest.pm
@@ -0,0 +1,105 @@
+package ACLTest;
+
+use strict;
+use warnings;
+
+use base 'TestBase';
+
+use Test::More;
+use Test::Fatal;
+
+use Lamework::ACL;
+
+sub denied_by_default : Test {
+    my $self = shift;
+
+    my $acl = $self->_build_acl;
+
+    ok !$acl->is_allowed('admin', 'login');
+}
+
+sub allow_allowed_action : Test {
+    my $self = shift;
+
+    my $acl = $self->_build_acl;
+
+    $acl->add_role('admin');
+    $acl->allow('admin', 'foo');
+
+    ok $acl->is_allowed('admin', 'foo');
+}
+
+sub deny_unknown_role : Test {
+    my $self = shift;
+
+    my $acl = $self->_build_acl;
+
+    ok !$acl->is_allowed('admin', 'foo');
+}
+
+sub deny_unknown_action : Test {
+    my $self = shift;
+
+    my $acl = $self->_build_acl;
+
+    $acl->add_role('admin');
+    $acl->allow('admin', 'foo');
+
+    ok !$acl->is_allowed('admin', 'bar');
+}
+
+sub deny_denied_action : Test {
+    my $self = shift;
+
+    my $acl = $self->_build_acl;
+
+    $acl->add_role('admin');
+    $acl->allow('admin', 'foo');
+    $acl->deny('admin', 'bar');
+
+    ok !$acl->is_allowed('admin', 'bar');
+}
+
+sub allow_everything_with_star : Test {
+    my $self = shift;
+
+    my $acl = $self->_build_acl;
+
+    $acl->add_role('admin');
+    $acl->allow('admin', '*');
+
+    ok $acl->is_allowed('admin', 'foo');
+}
+
+sub deny_action_despite_of_star : Test {
+    my $self = shift;
+
+    my $acl = $self->_build_acl;
+
+    $acl->add_role('admin');
+    $acl->allow('admin', '*');
+    $acl->deny('admin', 'foo');
+
+    ok !$acl->is_allowed('admin', 'foo');
+}
+
+sub inherit_rules : Test {
+    my $self = shift;
+
+    my $acl = $self->_build_acl;
+
+    $acl->add_role('user');
+    $acl->allow('user', 'foo');
+
+    $acl->add_role('admin', 'user');
+
+    ok $acl->is_allowed('admin', 'foo');
+}
+
+sub _build_acl {
+    my $self = shift;
+
+    return Lamework::ACL->new(@_);
+}
+
+1;
