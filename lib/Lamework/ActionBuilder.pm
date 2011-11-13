43c753b7f1ac2265d736dc12c154639d700ff6a9
vti <viacheslav.t@gmail.com>
2011-11-13 12:58:30 +0200

    Extracted factory

--- lib/Lamework/ActionBuilder.pm
+++ lib/Lamework/ActionBuilder.pm
@@ -3,54 +3,6 @@ package Lamework::ActionBuilder;
 use strict;
 use warnings;
 
-use base 'Lamework::Base';
-
-use Class::Load       ();
-use String::CamelCase ();
-use Try::Tiny;
-
-sub BUILD {
-    my $self = shift;
-
-    $self->{namespace} = '' unless defined $self->{namespace};
-
-    $self->{default_args} ||= {};
-}
-
-sub build {
-    my $self = shift;
-    my ($action, @args) = @_;
-
-    my $class = $self->_build_class_name($action);
-
-    return try {
-        Class::Load::load_class($class);
-
-        return $self->_build_action($class, %{$self->{default_args}}, @args);
-    }
-    catch {
-        $class =~ s{::}{/}g;
-
-        die $_ unless $_ =~ m{^Can't locate $class\.pm in \@INC };
-
-        return;
-    };
-}
-
-sub _build_class_name {
-    my $self = shift;
-    my ($action) = @_;
-
-    $action = String::CamelCase::camelize($action);
-
-    return "$self->{namespace}$action";
-}
-
-sub _build_action {
-    my $self = shift;
-    my ($class, @args) = @_;
-
-    return $class->new(@args);
-}
+use base 'Lamework::Factory';
 
 1;
---
+++ lib/Lamework/Factory.pm
@@ -0,0 +1,58 @@
+package Lamework::Factory;
+
+use strict;
+use warnings;
+
+use base 'Lamework::Base';
+
+use Class::Load       ();
+use String::CamelCase ();
+use Try::Tiny;
+
+sub BUILD {
+    my $self = shift;
+
+    $self->{namespace} = '' unless defined $self->{namespace};
+
+    $self->{default_args} ||= {};
+}
+
+sub build {
+    my $self = shift;
+    my ($name, @args) = @_;
+
+    my $class = $self->_build_class_name($name);
+
+    return try {
+        Class::Load::load_class($class);
+
+        return $self->_build_object($class, %{$self->{default_args}}, @args);
+    }
+    catch {
+        $class =~ s{::}{/}g;
+
+        die $_ unless $_ =~ m{^Can't locate $class\.pm in \@INC };
+
+        die $_ if $self->{die_on_not_found};
+
+        return;
+    };
+}
+
+sub _build_class_name {
+    my $self = shift;
+    my ($action) = @_;
+
+    $action = String::CamelCase::camelize($action);
+
+    return "$self->{namespace}$action";
+}
+
+sub _build_object {
+    my $self = shift;
+    my ($class, @args) = @_;
+
+    return $class->new(@args);
+}
+
+1;
