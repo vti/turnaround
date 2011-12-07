b40bd0ed1faca039445252dff16c3a7c8dbdebdd
vti <viacheslav.t@gmail.com>
2011-12-08 00:10:28 +0200

    Added simple app skeleton

---
+++ lib/Lamework/SimpleApp.pm
@@ -0,0 +1,43 @@
+package Lamework::SimpleApp;
+
+use strict;
+use warnings;
+
+use base 'Lamework';
+
+use Lamework::Home;
+use Lamework::Routes;
+use Lamework::Dispatcher::Routes;
+use Lamework::ActionFactory;
+use Lamework::Renderer::Caml;
+use Lamework::Displayer;
+
+sub startup {
+    my $self = shift;
+
+    $self->add_middleware('RequestDispatcher',
+        dispatcher =>
+          Lamework::Dispatcher::Routes->new(routes => $self->_build_routes));
+
+    $self->add_middleware(
+        'ActionFactory',
+        action_factory => Lamework::ActionFactory->new(
+            namespace => ref($self) . '::Action::'
+        )
+    );
+
+    $self->add_middleware(
+        'ViewDisplayer',
+        displayer => Lamework::Displayer->new(
+            renderer => Lamework::Renderer::Caml->new(home => $self->{home})
+        )
+    );
+}
+
+sub _build_routes {
+    my $self = shift;
+
+    return Lamework::Routes->new;
+}
+
+1;
--- t/lib/MyApp.pm
+++ t/lib/MyApp.pm
@@ -3,37 +3,17 @@ package MyApp;
 use strict;
 use warnings;
 
-use base 'Lamework';
+use base 'Lamework::SimpleApp';
 
 use Lamework::Home;
 use Lamework::Routes;
-use Lamework::Dispatcher::Routes;
-use Lamework::ActionFactory;
-use Lamework::Renderer::Caml;
-use Lamework::Displayer;
 
 sub startup {
     my $self = shift;
 
     $self->{home} = Lamework::Home->new(path => 't');
 
-    $self->add_middleware('RequestDispatcher',
-        dispatcher =>
-          Lamework::Dispatcher::Routes->new(routes => $self->_build_routes));
-
-    $self->add_middleware(
-        'ActionFactory',
-        action_factory => Lamework::ActionFactory->new(
-            namespace => ref($self) . '::Action::'
-        )
-    );
-
-    $self->add_middleware(
-        'ViewDisplayer',
-        displayer => Lamework::Displayer->new(
-            renderer => Lamework::Renderer::Caml->new(home => $self->{home})
-        )
-    );
+    $self->SUPER::startup;
 }
 
 sub _build_routes {
