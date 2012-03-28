ab41985284b3a2b2df48ae23e2ca4fb5ca44bb27
vti <viacheslav.t@gmail.com>
2012-03-28 23:45:30 +0300

    Correcly set user

--- t/lib/MiddlewareUserTest.pm
+++ t/lib/MiddlewareUserTest.pm
@@ -19,7 +19,8 @@ sub set_anonymous_when_no_session : Test {
 
     my $res = $mw->call($env);
 
-    is($env->{user}->role, 'anonymous');
+    $env = Lamework::Env->new($env);
+    is($env->get('user')->role, 'anonymous');
 }
 
 sub set_anonymous_when_session_but_no_user : Test {
@@ -31,7 +32,8 @@ sub set_anonymous_when_session_but_no_user : Test {
 
     my $res = $mw->call($env);
 
-    is($env->{user}->role, 'anonymous');
+    $env = Lamework::Env->new($env);
+    is($env->get('user')->role, 'anonymous');
 }
 
 sub set_anonymous_when_user_not_found : Test {
@@ -43,7 +45,8 @@ sub set_anonymous_when_user_not_found : Test {
 
     my $res = $mw->call($env);
 
-    is($env->{user}->role, 'anonymous');
+    $env = Lamework::Env->new($env);
+    is($env->get('user')->role, 'anonymous');
 }
 
 sub set_user : Test {
@@ -55,7 +58,8 @@ sub set_user : Test {
 
     my $res = $mw->call($env);
 
-    is($env->{user}->role, 'user');
+    $env = Lamework::Env->new($env);
+    is($env->get('user')->role, 'user');
 }
 
 sub _build_middleware {
