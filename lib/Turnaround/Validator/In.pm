e3b1c267fd0253b9c33f86f577011de9ba60fbf2
vti <viacheslav.t@gmail.com>
2012-04-21 16:36:40 +0300

    Added in validator

---
+++ lib/Turnaround/Validator/In.pm
@@ -0,0 +1,15 @@
+package Turnaround::Validator::In;
+
+use strict;
+use warnings;
+
+use base 'Turnaround::Validator::Base';
+
+sub is_valid {
+    my $self = shift;
+    my ($value, $in) = @_;
+
+    return !!grep {$value eq $_} @$in;
+}
+
+1;
