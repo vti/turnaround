b2e45e0498dddbc96879fbb55bedfde0b06d1c77
vti <viacheslav.t@gmail.com>
2012-07-01 13:47:52 +0300

    Added mailer plugin

---
+++ lib/Turnaround/Plugin/Mailer.pm
@@ -0,0 +1,23 @@
+package Turnaround::Plugin::Mailer;
+
+use strict;
+use warnings;
+
+use base 'Turnaround::Base';
+
+use Turnaround::Mailer;
+
+sub BUILD {
+    my $self = shift;
+
+    $self->{service_name} ||= 'mailer';
+}
+
+sub startup {
+    my $self = shift;
+
+    my $mailer = Turnaround::Mailer->new(%{$self->{config} || {}});
+    $self->{services}->register($self->{service_name} => $mailer);
+}
+
+1;
