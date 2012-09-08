4fb56688a16e11602f31b3fc5921447c85d3a987
vti <viacheslav.t@gmail.com>
2012-09-09 01:10:26 +0300

    Restore config test

---
+++ t/core/ConfigTest.pm
@@ -0,0 +1,30 @@
+package ConfigTest;
+
+use strict;
+use warnings;
+use utf8;
+
+use base 'TestBase';
+
+use Test::More;
+use Test::Fatal;
+
+use Turnaround::Config;
+
+sub load_config_base_on_extension : Test {
+    my $self = shift;
+
+    my $config = $self->_build_config;
+
+    my $data = $config->load('t/core/ConfigTest/config.yml');
+
+    is_deeply($data, {foo => 'bar', 'привет' => 'там'});
+}
+
+sub _build_config {
+    my $self = shift;
+
+    return Turnaround::Config->new(@_);
+}
+
+1;
