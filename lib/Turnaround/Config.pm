955b2fd761ce39e00868013417d3c3937e97387a
vti <viacheslav.t@gmail.com>
2014-12-14 18:46:09 +0200

    simplified

--- lib/Turnaround/Config.pm
+++ lib/Turnaround/Config.pm
@@ -27,13 +27,31 @@ sub load {
     my $self = shift;
     my ($path) = @_;
 
-    if ($self->{mode}) {
-        if ((my $mode = $ENV{PLACK_ENV}) && $ENV{PLACK_ENV} ne 'production') {
-            $mode = 'dev' if $mode eq 'development';
+    $path = $self->_change_based_on_mode($path) if $self->{mode};
 
-            $path =~ s{\.([^\.]+)$}{.$mode.$1};
-        }
+    my $class = $self->_detect_type($path);
+
+    my $config = slurp($path, $self->{encoding});
+
+    return $class->new->parse($config);
+}
+
+sub _change_based_on_mode {
+    my $self = shift;
+    my ($path) = @_;
+
+    if ((my $mode = $ENV{PLACK_ENV}) && $ENV{PLACK_ENV} ne 'production') {
+        $mode = 'dev' if $mode eq 'development';
+
+        $path =~ s{\.([^\.]+)$}{.$mode.$1};
     }
+
+    return $path;
+}
+
+sub _detect_type {
+    my $self = shift;
+    my ($path) = @_;
 
     my $basename = File::Basename::basename($path);
     my ($ext) = $basename =~ m{\.([^\.]+)$};
@@ -44,9 +62,7 @@ sub load {
 
     Turnaround::Loader->new->load_class($class);
 
-    my $config = slurp($path, $self->{encoding});
-
-    return $class->new->parse($config);
+    return $class;
 }
 
 1;
